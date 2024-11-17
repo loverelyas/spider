4a4d255d2f4da6f760339fcc5d9b091a8f5a09f2
loverelyas <129636393+loverelyas@users.noreply.github.com>
2024-11-17 03:06:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spider_ai",
-    version="0.6", 
+    version="0.5", 
     description="my lib is power",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
